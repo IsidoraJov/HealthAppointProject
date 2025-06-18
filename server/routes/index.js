cc5dfb01431cef727413d671fceb826f4aff6e3e
IsidoraJov <isidorajov97@gmail.com>
2025-06-18 03:58:59 +0200

    Set CORS for deployed frontend

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,14 +11,14 @@ const cors = require("cors");
 const CryptoJS = require("crypto-js");
 
 router.use(cors({
-  origin: 'http://localhost:3000' ,
+  origin: 'https://healthappoint.onrender.com' ,
   credentials: true,  
 }));
 
 router.use(cookieParser());
 // Ručni CORS kao dodatak
 router.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://healthappoint.onrender.com");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
